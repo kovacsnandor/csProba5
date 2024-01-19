ae1dca5d840650dffcc3b837aca6c55a93e66c1c
kovacsnandor <kovacsnandor@gmail.com>
2024-01-19 09:29:50 +0100

    projekt létrehozása

---
+++ Proba/Proba/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.ReadAllText("valami.txt"));
+
+            Console.ReadLine();
+        }
+    }
+}
